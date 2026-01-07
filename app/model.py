654e85b8bfb5a807869f681a90db95e576fe4f82
Kyra-pranav <pranav.bhardwaj@kyratechnologies.dev>
2026-01-07 23:33:37 +0530

    Initial commit - AI_Platform

---
+++ app/model.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def chatbot_response(user_text):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": user_text}
+        ]
+    )
+    return response.choices[0].message.content
